1645ae560ddc36d9931ef9feee66d38798578bfe
Harun Felicianne <123123333+sudo-harun@users.noreply.github.com>
2023-06-20 14:43:58 +0100

    Update install.sh

--- bspwm-config/install.sh
+++ bspwm-config/install.sh
@@ -2,21 +2,36 @@
 
 # Installing yay
 cd ~
-git clone https://github.com/Jguer/yay
+git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
 # Installing other packages
 cd ~
-yay -S bspwm sxhkd xf86-video-intel wine-staging winetricks neovim lightdm lightdm-webkit2-greeter lightdm-webkit-theme-litarvan lightdm-webkit2-theme-glorious ueberzug udisks2 unrar unzip ttf-jetbrains-mono-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd steam sof-firmware rofi rofi-emoji scrot dunst picom-ftlabs-git polkit-gnome ranger reflector-simple playerctl pavucontrol pamixer p7zip noto-fonts noto-fonts-cjk noto-fonts-emoji-apple neofetch mpv lxappearance iw htop gimp eww feh dragon-drop deluge-gtk discord ccache brillo alsa-firmware alsa-ucm-conf arandr bluez capitaine-cursors wmctrl xdg-desktop-portal xdg-desktop-portal-gtk zip upower ttf-roboto filezilla xdotool xprop
+yay -S wine-staging winetricks neovim lightdm lightdm-webkit2-greeter lightdm-webkit-theme-litarvan lightdm-webkit2-theme-glorious ueberzug udisks2 unrar unzip ttf-jetbrains-mono-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd steam sof-firmware rofi rofi-emoji scrot dunst picom-ftlabs-git polkit-gnome ranger reflector-simple playerctl pavucontrol pamixer p7zip noto-fonts noto-fonts-cjk noto-fonts-emoji-apple neofetch mpv lxappearance iw htop gimp feh dragon-drop deluge-gtk discord ccache brillo alsa-firmware alsa-ucm-conf arandr bluez capitaine-cursors wmctrl xdg-desktop-portal xdg-desktop-portal-gtk zip upower ttf-roboto filezilla xdotool xorg-xprop
 
 # Setup dotfiles
 cd ~/dotfiles/bspwm-config
 mv * ~/.config/
+
+# Enabling execution permissions
+cd ~/.config
+chmod +x bspwm/bspwmrc
+chmod +x bspwm/eww-bar-fullscreen.sh
+chmod +x bspwm/scrot.sh
+chmod +x sxhkd/sxhkdrc
+chmod +x dunst/dunstrc
+chmod +x ranger/scope.sh
+chmod +x scripts/ffmpeg.sh
+chmod +x scripts/scrot.sh
+chmod +x wmstuff/minimizer.sh
 
 # Finish
 clear
 mpv ~/.config/assets/notifications/sfx/retro-game-notification.wav
 echo "Complete by setting up lightdm"
 echo "and installing zsh"
-sleep 10
+sleep 3
+echo "Rebooting..."
+sleep 2
+reboot
