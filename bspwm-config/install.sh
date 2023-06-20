9e5568f24701752bc2cdcbbb4144ae57a6781582
Harun Felicianne <123123333+sudo-harun@users.noreply.github.com>
2023-06-20 18:22:47 +0100

    Update install.sh

--- bspwm-config/install.sh
+++ bspwm-config/install.sh
@@ -8,7 +8,7 @@ makepkg -si
 
 # Installing other packages
 cd ~
-yay -S wine-staging winetricks neovim lightdm lightdm-webkit2-greeter lightdm-webkit-theme-litarvan lightdm-webkit2-theme-glorious ueberzug udisks2 unrar unzip ttf-jetbrains-mono-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd steam sof-firmware rofi rofi-emoji scrot dunst picom-ftlabs-git polkit-gnome ranger reflector-simple playerctl pavucontrol pamixer p7zip noto-fonts noto-fonts-cjk noto-fonts-emoji-apple neofetch mpv lxappearance iw htop gimp feh dragon-drop deluge-gtk discord ccache brillo alsa-firmware alsa-ucm-conf arandr bluez capitaine-cursors wmctrl xdg-desktop-portal xdg-desktop-portal-gtk zip upower ttf-roboto filezilla xdotool xorg-xprop
+yay -S zsh wine-staging winetricks neovim lightdm lightdm-webkit2-greeter lightdm-webkit-theme-litarvan lightdm-webkit2-theme-glorious ueberzug udisks2 unrar unzip ttf-jetbrains-mono-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd steam sof-firmware rofi rofi-emoji scrot dunst picom-jonaburg-git polkit-gnome ranger reflector-simple playerctl pavucontrol pamixer p7zip noto-fonts noto-fonts-cjk noto-fonts-emoji-apple neofetch mpv lxappearance-gtk3 iw htop gimp feh dragon-drop deluge-gtk discord ccache brillo alsa-firmware alsa-ucm-conf arandr bluez capitaine-cursors wmctrl zip upower ttf-roboto filezilla xdotool xorg-xprop
 
 # Setup dotfiles
 cd ~/dotfiles/bspwm-config
