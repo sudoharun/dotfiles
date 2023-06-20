d64ef191025247a600b3bdf3888b3d814ee06be2
Harun Felicianne <123123333+sudo-harun@users.noreply.github.com>
2023-06-20 18:35:10 +0100

    Update install.sh

--- bspwm-config/install.sh
+++ bspwm-config/install.sh
@@ -8,7 +8,7 @@ makepkg -si
 
 # Installing other packages
 cd ~
-yay -S zsh neovim lightdm lightdm-webkit2-greeter lightdm-webkit-theme-litarvan lightdm-webkit2-theme-glorious ueberzug udisks2 unrar unzip ttf-jetbrains-mono-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd sof-firmware rofi rofi-emoji scrot dunst picom-jonaburg-git polkit-gnome ranger reflector-simple playerctl pavucontrol pamixer p7zip noto-fonts noto-fonts-cjk noto-fonts-emoji-apple neofetch mpv lxappearance-gtk3 iw htop gimp feh dragon-drop deluge-gtk discord ccache brillo alsa-firmware alsa-ucm-conf arandr bluez capitaine-cursors wmctrl zip upower ttf-roboto filezilla xdotool xorg-xprop
+yay -S kitty zsh neovim lightdm lightdm-webkit2-greeter lightdm-webkit-theme-litarvan lightdm-webkit2-theme-glorious ueberzug udisks2 unrar unzip ttf-jetbrains-mono-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd sof-firmware rofi rofi-emoji scrot dunst picom-jonaburg-git polkit-gnome ranger reflector-simple playerctl pavucontrol pamixer p7zip noto-fonts noto-fonts-cjk noto-fonts-emoji-apple neofetch mpv lxappearance-gtk3 iw htop gimp feh dragon-drop deluge-gtk discord ccache brillo alsa-firmware alsa-ucm-conf arandr bluez capitaine-cursors wmctrl zip upower ttf-roboto filezilla xdotool xorg-xprop
 
 # Setup dotfiles
 mkdir ~/.config
@@ -31,7 +31,6 @@ chmod +x wmstuff/minimizer.sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Finish
-mpv ~/.config/assets/notifications/sfx/retro-game-notification.wav
 echo "Complete by setting up lightdm"
 echo "and installing zsh"
 echo " "
