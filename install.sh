b5068ab6a88a73d6b2785f761702f78ce92cc958
sudo-harun <harunfelicianne@gmail.com>
2023-06-22 16:47:34 +0100

    Rearranged some files

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Installing yay
+cd ~
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# Installing other packages
+cd ~
+yay -S kitty zsh neovim firefox-developer-edition lightdm lightdm-webkit2-greeter lightdm-webkit-theme-litarvan lightdm-webkit2-theme-glorious ueberzug udisks2 unrar unzip ttf-jetbrains-mono-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd sof-firmware rofi rofi-emoji scrot dunst picom-jonaburg-git polkit-gnome ranger reflector-simple playerctl pavucontrol pamixer p7zip noto-fonts noto-fonts-cjk noto-fonts-emoji-apple neofetch mpv lxappearance-gtk3 iw htop gimp feh dragon-drop deluge-gtk discord ccache brillo alsa-firmware alsa-ucm-conf arandr bluez capitaine-cursors wmctrl zip upower ttf-roboto filezilla xdotool xorg-xprop xorg-xsetroot xdg-desktop-portal
+
+# Setup dotfiles
+mkdir ~/.config
+cd ~/dotfiles/bspwm-config
+mv * ~/.config/
+
+# Enabling execution permissions
+cd ~/.config
+chmod +x bspwm/bspwmrc
+chmod +x bspwm/fullscreen.sh
+chmod +x bspwm/scrot.sh
+chmod +x sxhkd/sxhkdrc
+chmod +x dunst/dunstrc
+chmod +x ranger/scope.sh
+chmod +x scripts/ffmpeg.sh
+chmod +x scripts/scrot.sh
+chmod +x wmstuff/minimizer.sh
+
+# Setup ohmyzsh
+cd ~
+echo "Last step is setting up ohmyzsh"
+sleep 3
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
