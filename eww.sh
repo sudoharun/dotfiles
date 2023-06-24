1567553f8ce8f2be51d36c34b71169e10eaa31d2
Harun Felicianne <123123333+sudo-harun@users.noreply.github.com>
2023-06-24 19:32:38 +0100

    Update eww.sh

--- eww.sh
+++ eww.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 cd ~
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
 rustup install nightly
 git clone https://github.com/elkowar/eww
 mv ~/eww ~/.eww
 cd ~/.eww
+rustup default nightly
 cargo build --release --no-default-features --features x11
 cd target/release
 chmod +x ./eww
 mkdir -p ~/.local/bin
 cp eww ~/.local/bin/
+rustup default stable
