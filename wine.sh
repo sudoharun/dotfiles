baa8bd8b9b9efb39e3bc0b698d431591bc400504
Harun Felicianne <123123333+sudo-harun@users.noreply.github.com>
2023-06-27 11:49:38 +0100

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+clear
+echo "WARNING: This script is intended to run inside a GUI (e.g. a window manager or desktop environment)"
+echo "If you are not in a GUI, please cancel by pressing Ctrl + c immediately"
+sleep 10
+clear
+echo "Continuing..."
+sleep 3
+clear
+
 cd ~
 yay -S wine-staging winetricks dxvk-bin
 sudo winetricks --self-update
@@ -9,4 +18,5 @@ winetricks -q vcrun2005
 winetricks -q d3dcompiler_43 d3dx11_42 d3dx11_43
 winetricks -q gfw msasn1 physx
 winetricks -q xact_x64 xact xinput
+winetricks -q wmp11
 WINEPREFIX=~/.wine setup_dxvk install
