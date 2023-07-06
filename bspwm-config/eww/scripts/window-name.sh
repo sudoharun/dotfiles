d63df2fb70a847ba16b4206c52309f5e31222c59
Harun Felicianne <123123333+sudo-harun@users.noreply.github.com>
2023-07-06 16:15:32 +0100

    Update window-name.sh

--- bspwm-config/eww/scripts/window-name.sh
+++ bspwm-config/eww/scripts/window-name.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 name() {
   winame=`xdotool getactivewindow getwindowname`
+
+  # Uncommment for character limit
+  # And change line 12 variable name
   # windowname=`echo ${winame::150}`
+  
   if [[ $winame == "" ]]; then
     echo "Desktop"
   else
