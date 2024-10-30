329c14066760d6f17f67282a2bebbd3abcb77371
Harun <123123333+sudoharun@users.noreply.github.com>
2024-10-30 13:18:11 +0000

    fix start_astal.sh

--- config/hypr/start_astal.sh
+++ config/hypr/start_astal.sh
@@ -2,10 +2,12 @@
 
 while [[ $(astal -l | grep "bar" | wc -l) < 1 ]]; do
   python $HOME/.config/astal/bar.py &
+  sleep 0.5
 done
 
 while [[ $(astal -l | grep "notifications" | wc -l) < 1 ]]; do
   python $HOME/.config/astal/notifications.py &
+  sleep 0.5
 done
 
-echo found
+echo done
